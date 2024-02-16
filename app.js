33b78fde566d7c684015f17b87c107b106dda5c8
Dorian Peters <dpeters08@gmail.com>
2024-02-16 15:25:12 -0800

    Add javascript code to create square grid, made container exact size so it doesn't grow or shrink

---
+++ app.js
@@ -0,0 +1,19 @@
+let squareSize = 16;
+let totalSize = squareSize * 10;
+let container = document.querySelector('.container');
+container.style.width = totalSize + 'px';
+container.style.height = totalSize + 'px';
+
+for(let i = 0; i < squareSize ** 2; i++) {
+  let div = document.createElement('div');
+  div.style.height = '8px';
+  div.style.width = '8px';
+  div.style.backgroundColor = 'white';
+  div.style.border = 'thin solid blue';
+  container.appendChild(div);
+
+  // Event listener not working
+  div.addEventListener('hover', () => {
+    div.style.backgroundColor = 'red';
+  });
+}
