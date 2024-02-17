5469ca316bd399d8f0fe8d1726e3e60f82e57fbb
Dorian Peters <dpeters08@gmail.com>
2024-02-16 18:35:19 -0800

    Implement button and resize, but does not meet specs

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-let squareSize = 16;
-let totalSize = squareSize * 10;
-let container = document.querySelector('.container');
-container.style.width = totalSize + 'px';
-container.style.height = totalSize + 'px';
+let initialSize = 16;
+resizeSquare(initialSize);
 
-for(let i = 0; i < squareSize ** 2; i++) {
-  let div = document.createElement('div');
-  div.style.height = '8px';
-  div.style.width = '8px';
-  div.style.backgroundColor = 'white';
-  div.style.border = 'thin solid blue';
-  container.appendChild(div);
-  
-  // Event listener not working
-  div.addEventListener('mouseover', () => {
-    div.style.backgroundColor = 'red';
-  });
+function resizeSquare (size = initialSize) {
+  let totalSize = size * 10;
+  let container = document.querySelector('.container');
+  container.style.width = totalSize + 'px';
+  container.style.height = totalSize + 'px';
+
+  for(let i = 0; i < size ** 2; i++) {
+    let div = document.createElement('div');
+    div.style.height = '8px';
+    div.style.width = '8px';
+    div.style.backgroundColor = 'white';
+    div.style.border = 'thin solid blue';
+    container.appendChild(div);
+    
+    div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = 'red';
+    });
+  }
 }
+
+let button = document.querySelector('button');
+button.addEventListener('click', () => {
+  let newSize = prompt("What size should the square be? (Max: 100)");
+  let container = document.querySelector('.container');
+  container.replaceChildren();
+  resizeSquare(newSize);
+})
